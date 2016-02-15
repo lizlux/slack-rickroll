af66fd9ca660b3b7892aa78abb8dfc4d56bec4b5
lizlux <liz@lizlux.net>
2016-02-14 22:04:36 -0800

    ws

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res) {
 	res.send('Hello Slack!\n');
 });
+
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
 });
